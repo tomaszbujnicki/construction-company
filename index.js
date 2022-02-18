e830bc72e4a38ececc619e3adc4e6b68560b33df
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2022-02-18 16:46:42 +0100

    Add favicon, menu links

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import * as bootstrap from 'bootstrap';
+
+const navbar = document.getElementById('nav');
+
+function doSomething() {
+  if (window.scrollY > 300) navbar.classList.remove('top');
+  if (window.scrollY < 200) navbar.classList.add('top');
+}
+
+document.addEventListener('scroll', doSomething);
